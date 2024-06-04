fedd3fcaa39942810f89789ded347887772eb6f4
Vinithapatel1211 <vxp40370@ucmo.edu>
2024-06-04 18:53:26 -0500

    Demo Project

---
+++ Demo1.java
@@ -0,0 +1,19 @@
+package Demo;
+import java.util.*;
+public class Demo1 {
+	
+	public static void main(String[] args) {
+		
+		List<String> l = new ArrayList<>();
+		l.add("Navya");
+		l.add("varshi");
+		l.add("astha");
+		
+		for(int index=0; index < l.size(); index++){
+			
+			System.out.println(l.get(index));
+			
+		}
+	}
+
+}
